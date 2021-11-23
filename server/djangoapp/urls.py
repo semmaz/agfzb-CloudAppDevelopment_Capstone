38dfc70b0626060e18790130c473e71e6871720c
Simon Mazur <semmaz.box@gmail.com>
2021-11-23 09:19:44 +0000

    Added Proxy Services

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,25 +9,28 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
-    # path for about view
+    # path for home view
+    path(route='', view=views.get_dealerships, name='index'),
+
+    # path for about us view
     path(route='about', view=views.about, name='about'),
 
     # path for contact us view
     path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
-    path('registration/', views.registration_request, name='registration'),
+    path('registration/', view=views.registration_request, name='registration'),
 
     # path for login
-    path('login/', views.login_request, name='login'),
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
-    path('logout/', views.logout_request, name='logout'),
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-
+    path('dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
+    
     # path for add a review view
+    path(route='addreview/<int:dealer_id>/', view=views.add_review, name='add_review')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
